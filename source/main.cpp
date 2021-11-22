c13ec442639527704d1c33d1908bda88af04a066
René Hansen <rene@hansen-audio.de>
2021-11-22 21:03:17 +0100

    Create randomized test values

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,8 @@
 #include "ha/fx_collection/trance_gate.h"
 #include <chrono>
 #include <iostream>
+#include <random>
+#include <vector>
 
 using namespace std::chrono;
 
@@ -32,8 +34,21 @@ struct Timer
 
 int main()
 {
-    const int NUM_ROUNDS = 10;
     const int NUM_BENCH_ROUNDS = 10000000;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0.0f, 1.0f);
+
+    std::vector<float> frames;
+    frames.reserve(NUM_BENCH_ROUNDS);
+
+    for (int n = 0; n < NUM_BENCH_ROUNDS; ++n)
+    {
+        frames.emplace_back(dis(gen));
+    }
+
+    const int NUM_ROUNDS = 4;
     for (auto j = 0; j < NUM_ROUNDS; j++)
     {
         std::cout << "Round " << j << std::endl;
@@ -45,7 +60,13 @@ int main()
             {
                 Timer timer{"Rust: "};
                 for (auto i = 0; i < NUM_BENCH_ROUNDS; i++)
+                {
+                    in[0] = frames[i];
+                    in[1] = frames[i];
                     ha::fx_collection_rs::TranceGateImpl::process(tg, &in, &out);
+
+                    // std::cout << out[0] << ", " << out[1] << std::endl;
+                }
             }
         }
 
@@ -58,7 +79,11 @@ int main()
                 Timer timer{"C++ : "};
                 for (auto i = 0; i < NUM_BENCH_ROUNDS; i++)
                 {
+                    in.data[0] = frames[i];
+                    in.data[1] = frames[i];
                     ha::fx_collection::TranceGateImpl::process(tg, in, out);
+
+                    // std::cout << out.data[0] << ", " << out.data[1] << std::endl;
                 }
             }
         }
